c57c96e28d52f61c9d63bad5df85ae9a1b495e0b
touros91 <andreageraci91@gmail.com>
2021-09-20 16:46:54 +0200

    Aggiunto bonus

--- js/main.js
+++ js/main.js
@@ -7,28 +7,50 @@
 // Aggiungere lo stato per ogni todo e modificare la visualizzazione dell'elemento in pagina, verde e barrato se il todo è stato fatto o rosso se non è ancora stato fatto.
 // Prevedere inoltre un pulsante per modificare lo stato del singolo todo
 
-
 const app = new Vue(
     {
         el: '#root',
         data: {
             todos: [
-                "Fare gli esercizi",
-                "Fare la spesa",
-                "Fare la torta",
-                "Fare le pulizie",
-                "Fare il bucato"
+                {
+                    task: "Fare gli esercizi",
+                    status: false
+                },
+                {
+                    task: "Fare la spesa",
+                    status: false
+                },
+                {
+                    task: "Fare la torta",
+                    status: false
+                },
+                {
+                    task: "Fare le pulizie",
+                    status: false
+                },
+                {
+                    task: "Fare il bucato",
+                    status: false
+                }
             ],
             newTodo: ""
         },
         methods: {
             addNewTodo: function (){
                 if(this.newTodo != ""){
-                    this.todos.push(this.newTodo);
+                    this.todos.push(
+                        {
+                        task: this.newTodo,
+                        status: false
+                        }
+                    );
                 }
             },
             removeTodo: function(index){
                 this.todos.splice(index, 1);
+            },
+            done: function(index){
+                this.todos[index].status = true;
             }
         }
     }
